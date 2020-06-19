7e1a00903d71587a0e0fb1afe40a0c4a8b4563c7
Pedro Sousa <sousacmpedrogmail.com>
2020-06-19 08:22:49 -0300

    Inicio de codificação do footer

--- include/footer.php
+++ include/footer.php
@@ -1,4 +1,7 @@
-
+<div class="container footer">
+    <h2 class="text-center">Use it free!</h2>
+    <p class="text-center">"Cras dolor mi, tempus sit amet iaculis at, tristique ut urna. Etiam ipsum erat, efficitur ac dui vitae, pharetra iaculis nisi."</p>
+</div>
 <script type="text/javascript" src="assets/js/pooper.js"></script>
 <script type="text/javascript" src="assets/js/bootstrap.js"></script>
 <script type="text/javascript" src="assets/js/script.js"></script>
