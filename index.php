8f331abef0b0b04e8f9c8858152727e51e6d843c
Pedro Sousa <sousacmpedrogmail.com>
2020-06-19 07:32:16 -0300

    One Big Column

--- index.php
+++ index.php
@@ -3,4 +3,5 @@
 <?php require 'include/food.php'; ?>
 <?php require 'include/jour.php'; ?>
 <?php require 'include/nature.php'; ?>
+<?php require 'include/nightlife.php'; ?>
 <?php require 'include/footer.php'; ?>
