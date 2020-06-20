0c37b80d29f9a9ede5e3a2c942b72248eaab75d3
Pedro Sousa <sousacmpedrogmail.com>
2020-06-19 22:21:14 -0300

    Ajustes no footer.php e style.css

--- include/footer.php
+++ include/footer.php
@@ -1,11 +1,14 @@
-<div class="container conteudo">
+<div class="container conteudo footer">
     <h2 class="text-center">Use it free!</h2>
     <p class="text-center">"Cras dolor mi, tempus sit amet iaculis at, tristique ut urna. Etiam ipsum erat, efficitur ac dui vitae, pharetra iaculis nisi."</p>
 </div>
-<hr/>
-<h6 class="text-center font-weight-bold"> Copyright © <?php echo date('Y'); ?> <a href="https://pedlets.com.br" class="text-reset" target="_blank">PEDLETS DEVS</a> Todos os direitos reservados</h6>
+<hr />
+<div class="footer">
+    <h6 class="text-center font-weight-bold"> Copyright © <?php echo date('Y'); ?> <a href="https://pedlets.com.br" class="text-reset" target="_blank">PEDLETS DEVS</a> Todos os direitos reservados</h6>
+</div>
 <script type="text/javascript" src="assets/js/pooper.js"></script>
 <script type="text/javascript" src="assets/js/bootstrap.js"></script>
 <script type="text/javascript" src="assets/js/script.js"></script>
 </body>
+
 </html>
